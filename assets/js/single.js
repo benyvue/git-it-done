284c8d117f19b95998617417295ac93b15d1c9d0
MightyBenno <88509973+benyvue@users.noreply.github.com>
2021-09-30 14:29:30 -0500

    coded to add link to extra issues not listed on page

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 
 
@@ -10,6 +11,11 @@ var getRepoIssues = function(repo) {
     if (response.ok) {
       response.json().then(function(data){
         displayIssues(data);
+
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo)
+        }
       });
     }
     else {
@@ -18,7 +24,25 @@ var getRepoIssues = function(repo) {
   });
 }
 
-getRepoIssues("benyvue/bens-portfolio");
+
+
+var displayWarning = function(repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("targt", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
+
+
+
+getRepoIssues("facebook/react") 
+
 
 
 
